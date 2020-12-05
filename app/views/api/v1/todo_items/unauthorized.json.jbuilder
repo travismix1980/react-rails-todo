d1792d3735a2829435f3a53eac392f2745851474
Travis Mix <travismix1980@gmail.com>
2020-12-04 17:03:13 -0800

    backend mostly complete next step add react front end

---
+++ app/views/api/v1/todo_items/unauthorized.json.jbuilder
@@ -0,0 +1,2 @@
+# app/views/api/v1/todo_items/unauthorized.json.jbuilder
+json.error "You are not authorized to perform this action."
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     root 'pages#my_todo_items', as: :authenticated_root
   end
   root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :todo_items, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
